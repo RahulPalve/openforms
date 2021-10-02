783d01ab9f945b1f034fec74361cbb92c5f46240
RahulPalve <hi.rahulpalve@gmail.com>
2021-10-02 10:23:20 +0530

    feat(integration): google sheet support

--- openforms/integrations/google_sheets/__init__.py
+++ openforms/integrations/google_sheets/__init__.py
@@ -1,8 +1,23 @@
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+from oauth2client import client
 
-scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 
-creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+class GoogleSheetIntegration:
+    def __init__(self, sheet_id):
+        self.client = gspread.service_account(
+            filename="openforms/integrations/google_sheets/credentials.json"
+        )
+        self.spreadsheet = self.client.open_by_key(
+            sheet_id
+        )  # sheet access must be given to the gcloud service account
+        self.worksheet = self.set_worksheet()
 
-gsheets = gspread.authorize(creds)
+    def set_worksheet(self, wsheet_number=0):
+        worksheet = self.spreadsheet.get_worksheet(wsheet_number)
+        return worksheet
+
+    def add_row(self, data):
+        self.worksheet.append_row(data)
+
+    def set_header(self, data):
+        self.worksheet.insert_row(data, 1)
--- openforms/integrations/google_sheets/test.py
+++
@@ -1,4 +0,0 @@
-from google_sheets import gsheets
-
-spreadsheet = gsheets.open("openforms_test")
-worksheet = spreadsheet.add_worksheet(title="A worksheet", rows="100", cols="20")
